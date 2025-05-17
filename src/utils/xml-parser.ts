993b3142da5e7d00963340f7f4c37e17512f8d4a
Terijaki <590522+terijaki@users.noreply.github.com>
2025-05-17 10:53:32 +0200

    fix: incorrect type for team club

--- src/utils/xml-parser.ts
+++ src/utils/xml-parser.ts
@@ -7,7 +7,7 @@ export const xmlParser = new XMLParser({
 	ignorePiTags: true,
 	isArray: (name, path) => {
 		// Define paths that should always be arrays
-		const arrayPaths = ["teams.team", "matches.match", "rankings.ranking","rankings.ranking.team.club", "sportsclub.teams.team"];
+		const arrayPaths = ["teams.team", "matches.match","matches.match.referees.referee", "rankings.ranking","rankings.ranking.resultTypes", "sportsclub.teams.team"];
 		return arrayPaths.includes(path);
 	},
 });
